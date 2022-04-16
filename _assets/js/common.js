54ab405b62b7c75bdf9d6d631a31e98cabb2d22e
kokoro <cocosyu.11261126@gmail.com>
2022-04-17 00:06:48 +0900

    Add: スムーススクロールを追加

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -18,3 +18,25 @@
   addEventListener('resize', switchViewport, false);
   switchViewport();
 })();
+
+//スムーススクロール
+const urlHash = location.hash;
+
+window.addEventListener('DOMContentLoaded', () => {
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+
+  anchorLinksArr.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const targetId = link.hash;
+      const targetElement = document.querySelector(targetId);
+      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top - 60;
+
+      window.scrollTo({
+        top: targetOffsetTop,
+        behavior: "smooth"
+      });
+    });
+  });
+});
