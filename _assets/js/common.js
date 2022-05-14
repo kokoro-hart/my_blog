bc17b59180823dc7da98463c40b633858c590c26
kokoro <cocosyu.11261126@gmail.com>
2022-05-14 22:14:43 +0900

    Add: サイドバーの目次のスクロールイベントを追加

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -19,10 +19,10 @@
   switchViewport();
 })();
 
-//スムーススクロール
 const urlHash = location.hash;
 
 window.addEventListener('DOMContentLoaded', () => {
+  // スムーススクロール
   const anchorLinks = document.querySelectorAll('a[href^="#"]');
   const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
 
@@ -31,7 +31,7 @@ window.addEventListener('DOMContentLoaded', () => {
       e.preventDefault();
       const targetId = link.hash;
       const targetElement = document.querySelector(targetId);
-      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top - 60;
+      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top - document.documentElement.clientHeight / 2.5 ;
 
       window.scrollTo({
         top: targetOffsetTop,
@@ -39,4 +39,35 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     });
   });
+  
+  // サイドバーの目次の高さを動的に
+  const toc = document.querySelector('.p-toc-sidebar__inner');
+  const tocHeight = toc.offsetHeight;
+  
+  // 高さ90vh以上の場合以外は生成された目次の高さに調整
+  if (tocHeight >= (document.documentElement.clientHeight * 0.9)) {
+    toc.style.maxHeight = 90 + 'vh';
+  } else {
+    toc.style.maxHeight = `${tocHeight}px`;
+  }
 });
+
+// 表示中コンテンツに合わせて目次にfocusを当てる
+const wpContentHeading = document.querySelectorAll('.js-heading');
+const options = {
+  root: null,
+  rootMargin: '-50% 0px',
+  threshold: 0
+};
+const observer = new IntersectionObserver(followContents, options);
+wpContentHeading.forEach(img => {
+  observer.observe(img);
+});
+function followContents(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const newActiveIndex = document.querySelector(`[data-id='#${entry.target.id}']`);
+      newActiveIndex.focus();
+    }
+  });
+}
