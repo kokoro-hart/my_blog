6c50221f31c19837ea2dc986b812b73bfa9a1b50
kokoro <cocosyu.11261126@gmail.com>
2022-05-15 11:53:28 +0900

    Add: barba.jsを追加

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -2,6 +2,12 @@
  * @modules : node_modulesフォルダまでの絶対パスのエイリアス
  * webpack.config.jsにて定義している
  */
+import $ from "@modules/jquery";
+import barba from '@modules/@barba/core';
+import Prism from './lib/prism';
+import './modules/theme';
+
+Prism.highlightAll();
 
 //デバイス幅360px以下はリサイズして表示
 !(function () {
@@ -19,10 +25,9 @@
   switchViewport();
 })();
 
+// スムーススクロール
 const urlHash = location.hash;
-
-window.addEventListener('DOMContentLoaded', () => {
-  // スムーススクロール
+function initSmoothScroll() {
   const anchorLinks = document.querySelectorAll('a[href^="#"]');
   const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
 
@@ -31,7 +36,7 @@ window.addEventListener('DOMContentLoaded', () => {
       e.preventDefault();
       const targetId = link.hash;
       const targetElement = document.querySelector(targetId);
-      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top - document.documentElement.clientHeight / 2.5 ;
+      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top - document.documentElement.clientHeight / 2.5;
 
       window.scrollTo({
         top: targetOffsetTop,
@@ -39,35 +44,134 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     });
   });
-  
-  // サイドバーの目次の高さを動的に
+}
+
+// サイドバーの目次の高さを動的に
+function tuningSidebar() {
   const toc = document.querySelector('.p-toc-sidebar__inner');
-  const tocHeight = toc.offsetHeight;
-  
+  let tocHeight;
   // 高さ90vh以上の場合以外は生成された目次の高さに調整
-  if (tocHeight >= (document.documentElement.clientHeight * 0.9)) {
-    toc.style.maxHeight = 90 + 'vh';
-  } else {
-    toc.style.maxHeight = `${tocHeight}px`;
+  if (toc != null) {
+    tocHeight = toc.offsetHeight;
+
+    if (tocHeight >= (document.documentElement.clientHeight * 0.9)) {
+      toc.style.maxHeight = 90 + 'vh';
+    } else {
+      toc.style.maxHeight = `${tocHeight}px`;
+    }
+  }
+}
+
+// 表示中コンテンツに合わせて目次にfocusを当てる
+function followTocContents() {
+  const wpContentHeading = document.querySelectorAll('.js-heading');
+  const options = {
+    root: null,
+    rootMargin: '-50% 0px',
+    threshold: 0
+  };
+  const observer = new IntersectionObserver(followContents, options);
+  wpContentHeading.forEach(img => {
+    observer.observe(img);
+  });
+  function followContents(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const newActiveIndex = document.querySelector(`[data-id='#${entry.target.id}']`);
+        newActiveIndex.focus();
+      }
+    });
+  }
+}
+
+initSmoothScroll();
+followTocContents();
+tuningSidebar();
+
+// Contact Form 7を正常動作させるための関数
+function contactForm7Run(next) {
+  var cfSelector = 'div.wpcf7 > form';
+  var cfForms = $(next.container).find(cfSelector);
+  if (cfForms.length) {
+    $(cfSelector).each(function () {
+      var $form = $(this);
+      wpcf7.init($form[0]);
+    });
+  }
+}
+
+//barba遷移時のheadタグの書き換え
+const replaceHead = (data) => {
+  const head = document.head;
+  const newPageRawHead = data.next.html.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0];
+  const newPageHead = document.createElement('head');
+  newPageHead.innerHTML = newPageRawHead;
+
+  const removeHeadTags = [
+    "meta[name='keywords']"
+    , "meta[name='description']"
+    , "meta[property^='og']"
+    , "meta[name^='twitter']"
+    , "meta[itemprop]"
+    , "link[itemprop]"
+    , "link[rel='prev']"
+    , "link[rel='next']"
+    , "link[rel='canonical']"
+  ].join(',');
+
+  const headTags = head.querySelectorAll(removeHeadTags)
+
+  for (let i = 0; i < headTags.length; i++) {
+    head.removeChild(headTags[i]);
+  }
+
+  const newHeadTags = newPageHead.querySelectorAll(removeHeadTags)
+
+  for (let i = 0; i < newHeadTags.length; i++) {
+    head.appendChild(newHeadTags[i]);
+  }
+}
+
+//barba.init
+barba.init({
+  transitions: [{
+    beforeEnter({ next }) {
+      contactForm7Run(next);
+    },
+  }],
+
+  views: [
+    {
+      namespace: 'single',
+      beforeEnter(data) {
+        followTocContents();
+      }
+    },
+  ],
+
+  prevent: function (e) {
+    if (e.el.classList.contains('ab-item')) {
+      return true;
+    }
   }
 });
 
-// 表示中コンテンツに合わせて目次にfocusを当てる
-const wpContentHeading = document.querySelectorAll('.js-heading');
-const options = {
-  root: null,
-  rootMargin: '-50% 0px',
-  threshold: 0
-};
-const observer = new IntersectionObserver(followContents, options);
-wpContentHeading.forEach(img => {
-  observer.observe(img);
+barba.hooks.beforeEnter((data) => {
+  initSmoothScroll();
+  Prism.highlightAll();
+  replaceHead(data);
 });
-function followContents(entries) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      const newActiveIndex = document.querySelector(`[data-id='#${entry.target.id}']`);
-      newActiveIndex.focus();
-    }
-  });
-}
+
+barba.hooks.afterEnter(() => {
+  window.scrollTo(0, 0);
+});
+
+barba.hooks.after((data) => {
+  tuningSidebar();
+
+  //ページ遷移後にAnalyticsにトラッキングを送信
+  if (typeof ga === 'function') {
+    ga('set', 'page', window.location.pathname);
+    ga('send', 'pageview');
+  }
+});
--- _assets/js/main.js
+++ _assets/js/main.js
@@ -3,13 +3,7 @@
  * webpack.config.jsにて定義している
  */
 
-//SVGスプライトをIEで使用するためのライブラリ
-import '@modules/svgxuse';
 
-//PrismJS
-import './lib/prism';
 
-//ダークテーマ切り替え
-import './modules/theme.js';
-
+// メインスクリプト
 import './common'
