a794d76a1dcbe732899a2ce1c8321b0edc97dc82
kokoro <cocosyu.11261126@gmail.com>
2022-05-22 13:05:52 +0900

    Fix: meta情報を動的に修正 Add: ローダーを追加

--- wp/header.php
+++ wp/header.php
@@ -5,10 +5,14 @@
   <meta name="format-detection" content="telephone=no">
   <meta name="viewport" content="width=device-width">
   <meta name="description" content="<?php bloginfo('description'); ?>">
-  <meta property="og:title" content="<?php bloginfo( 'name' ); ?>">
+  <meta property="og:title" content="<?php the_title(); ?> - <?php bloginfo( 'name' ); ?>">
   <meta property="og:description" content="<?php bloginfo('description'); ?>">
   <meta property="og:url" content="<?php bloginfo('url'); ?>">
-  <meta property="og:type" content="website ">
+  <?php if(is_front_page()) : ?>
+  <meta property="og:type" content="website">
+  <?php else: ?>
+  <meta property="og:type" content="article">
+  <?php endif; ?>
   <meta property="og:local" content="ja_JP">
   <?php if (has_post_thumbnail()) : ?>
   <meta property="og:image" content="<?php the_post_thumbnail_url(); ?>" />
@@ -25,6 +29,9 @@
 
 <body data-barba="wrapper">
   <a class="skip-link" href="#main" tabindex="0">メインコンテンツにスキップ</a>
+  <div id="js-loading" class="p-loader">
+    <div class="p-loader__part"></div>
+  </div>
   <div class="site-wrapper">
     <header class="l-header p-header">
       <div class="p-header__inner l-container">
